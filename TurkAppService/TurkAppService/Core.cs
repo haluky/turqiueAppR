dd451f1ec882556ab04a1e12a738d3e1da1ab32b
Halukmy <Halukmy@DESKTOP-3LJ8040>
2018-02-25 14:31:55 +0300

    v2
    v2

--- TurkAppService/TurkAppService/Core.cs
+++ TurkAppService/TurkAppService/Core.cs
@@ -81,22 +81,45 @@ namespace TurkAppService
 
         internal class CrFirma
         {       // 
-
             public object _id { get; set; }
             public string FirmaID { get; set; }
             public string firmaAdi { get; set; }
             public string firmaLogo { get; set; }
             public string firmaType { get; set; }
             public IEnumerable<double> Coordinates { get; set; } //lat long
-
             public string firmaAdres { get; set; }
             public string firmaTelefon { get; set; }
-
-
             public IEnumerable<string> firmaUrunler { get; set; }
-
-
         }
+
+
+        internal class CrfirmaX
+        {
+
+
+            public string _id { get; set; }
+            //  (artikelnumber)
+            public string FirmaId { get; set; }
+            public string magzaId { get; set; }
+
+            public string _adresBilgisi { get; set; }
+            public string _telefon { get; set; }
+            public string _email { get; set; }
+            public double _latidX { get; set; }
+            public double _longtX { get; set; }
+
+            public string calismaSaatleri { get; set; }
+            public string SingleUrunBaslik { get; set; }
+
+            public double guncelFiyat { get; set; }
+            public double eskiFiyat { get; set; }
+
+            public bool isPremium { get; set; }
+            public uint tiklanmaDegeri { get; set; }
+
+            //public string SingleUrunBaslik{ get; set; }
+        }
+
 
 
 
